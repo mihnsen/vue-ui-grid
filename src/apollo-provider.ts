516ca7804373cfe26c3f5d27ede5368a726cdb2d
mihnsen <minhnt.hut@gmail.com>
2021-06-17 15:15:06 +0700

    Improve graphql connect

--- src/apollo-provider.ts
+++ src/apollo-provider.ts
@@ -1,13 +1,30 @@
 import Vue from 'vue'
 import VueApollo from 'vue-apollo'
-import ApolloClient from 'apollo-boost/lib/index'
+import { ApolloClient, HttpLink, ApolloLink, InMemoryCache } from 'apollo-boost'
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:3102/v1/graphql'
+})
+
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      'x-hasura-admin-secret': process.env.VUE_APP_GRAPHQL_SECRET
+    }
+  })
+
+  // Call the next link in the middleware chain.
+  return forward(operation)
+})
 
 Vue.use(VueApollo)
 
 const apolloClient = new ApolloClient({
-  // You should use an absolute URL here
-  // uri: 'https://api.graphcms.com/simple/v1/awesomeTalksClone'
-  uri: 'http://localhost:3102/v1/graphql'
+  cache: new InMemoryCache(),
+  link: ApolloLink.from([
+    authLink,
+    httpLink // Http link must be at the end of this array: https://stackoverflow.com/a/58736777
+  ])
 })
 
 const apolloProvider = new VueApollo({
